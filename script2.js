357a5079eeb3e50dbedf073b9c8b98f04b4fdba7
50/50 <80717571+50n50@users.noreply.github.com>
2025-06-24 22:40:09 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 async function searchResults(keyword) {
     try {
         const encodedKeyword = encodeURIComponent(keyword);
-        cconst responseText = await cloudflareFetch(`https://novellive.app/book/the-innkeeper/chapter-1-a-shooting-star-and-a-wish`, {
+        const responseText = await cloudflareFetch(`https://novellive.app/book/the-innkeeper/chapter-1-a-shooting-star-and-a-wish`, {
     moduleId: "testModule",
     headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
